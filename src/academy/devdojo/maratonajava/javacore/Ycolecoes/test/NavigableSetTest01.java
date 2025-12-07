55b97ea87eb74bcb2731ba3508426c24f82a242e
Jeferson Vieira <jefinhobr567@gmail.com>
2025-12-07 19:28:02 -0300

    Estudo completo de NavigableSet e TreeSet

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSetTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSetTest01.java
@@ -15,13 +15,20 @@ class SmartphoneMarcaComparator implements Comparator<Smartphone>{
     }
 }
 
+class MangaPrecoComparator implements Comparator<Manga>{
+
+    @Override
+    public int compare(Manga o1, Manga o2) {
+        return Double.compare(o1.getPreço(), o2.getPreço());
+    }
+}
 public class NavigableSetTest01 {
     public static void main(String[] args) {
         NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
         Smartphone smartphone = new Smartphone("123", "Nokia");
         set.add(smartphone);
 
-        NavigableSet<Manga> mangas = new TreeSet<>();
+        NavigableSet<Manga> mangas = new TreeSet<>(new MangaPrecoComparator());
         mangas.add(new Manga(5L, "Dragon Ball", 100));
         mangas.add(new Manga(3L, "Cavaleiros do Zodíaco", 90));
         mangas.add(new Manga(1L, "Yuyu Hakusho", 80));
