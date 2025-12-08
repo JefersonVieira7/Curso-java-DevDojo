5994a5d23539ee941fe5b6a2a41c7e8c965b770b
Jeferson Vieira <jeferson.vieira.dev@gmail.com>
2025-12-08 19:08:24 -0300

    Concluída a segunda parte de  Map, HashMap, LinkedHashMap

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest02.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest02.java
@@ -1,20 +1,41 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
 import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Consumidor;
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class MapTest02 {
     public static void main(String[] args) {
         HashSet<Consumidor> consumidores = new HashSet<>();
         Consumidor consumidor1 = new Consumidor("William Suane");
         Consumidor consumidor2 = new Consumidor("Jeferson");
+        Consumidor consumidor3 = new Consumidor("Indiano");
+
+        HashSet<Manga> mangas = new HashSet<>();
+
+        Manga manga1 = new Manga(5L, "Dragon Ball", 100, 0);
+        Manga manga2 = new Manga(3L, "Cavaleiros do Zodíaco", 90, 1);
+        Manga manga3 = new Manga(1L, "Yuyu Hakusho", 80, 0);
+
+        mangas.add(manga1);
+        mangas.add(manga2);
+        mangas.add(manga3);
+
         consumidores.add(consumidor1);
         consumidores.add(consumidor2);
+        consumidores.add(consumidor3);
         for (Consumidor consumidor : consumidores){
             System.out.println(consumidor);
+        }
+
+        Map<Consumidor, Manga> consumidorManga = new HashMap<>();
+        consumidorManga.put(consumidor1, manga1);
+        consumidorManga.put(consumidor2, manga3);
+        consumidorManga.put(consumidor3, manga2);
+
+        for (Map.Entry<Consumidor, Manga> entry : consumidorManga.entrySet()){
+            System.out.println(entry.getKey() + " - "+ entry.getValue());
         }
     }
 }
