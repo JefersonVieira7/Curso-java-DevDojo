0ba7cc892b2f2b774f694b2685b0018b944ff93f
Jeferson Vieira <jeferson.vieira.dev@gmail.com>
2025-12-26 23:30:57 -0300

    188 - Generics pt 06 - Métodos Genéricos aula concluída

--- src/academy/devdojo/maratonajava/javacore/Zgenerics/test/MetodoGenericoTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/test/MetodoGenericoTest01.java
@@ -4,13 +4,16 @@ import academy.devdojo.maratonajava.javacore.Fmodificadorestatico.Domain.Carro;
 import academy.devdojo.maratonajava.javacore.ZZEstream.test.Pessoa;
 import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Barco;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MetodoGenericoTest01 {
     public static void main(String[] args) {
-        new DoisAtributos<Carro, Pessoa, Barco>();
+        criarArrayComUmObjeto(new Barco("Canoa"));
     }
-    static class DoisAtributos <T,X, I>{
-        private T t;
-        private X x;
-        private I i;
+
+    private static <T> void criarArrayComUmObjeto(T t){
+        List<T> list = List.of(t);
+        System.out.println(list);
     }
 }
