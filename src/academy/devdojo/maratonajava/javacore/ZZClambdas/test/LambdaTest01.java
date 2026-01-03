4526504802ec6cc99d7f385dbeb7801ebfa92d04
Jeferson Vieira <jeferson.vieira.dev@gmail.com>
2026-01-03 14:39:40 -0300

    Conluído do estudo de Lambdas(Comsumer) aula 192

--- src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/LambdaTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/LambdaTest01.java
@@ -5,13 +5,14 @@ import java.util.function.Consumer;
 
 public class LambdaTest01 {
     public static void main(String[] args) {
-
+        List<String> names = List.of("Jeferson", "jefinho");
+        System.out.println("Tamanho da lista: "+ names.size());
+        foreach(names, name -> System.out.println(name));
     }
 
     public static <T> void foreach(List<T> list, Consumer<T> consumer){
-        for (T e : list) {
-            consumer.accept(e);
+        for (T element : list) {
+            consumer.accept(element);
         }
-
     }
 }
