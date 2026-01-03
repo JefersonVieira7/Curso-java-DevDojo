f131ef80159f19451de67e46d3880864fa793115
Jeferson Vieira <jeferson.vieira.dev@gmail.com>
2026-01-03 14:18:53 -0300

    revisando os estudos de lambadas(Predicate)

--- src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/interfaces/CarPredicate.java
@@ -2,6 +2,6 @@ package academy.devdojo.maratonajava.javacore.ZZBcomportamento.interfaces;
 import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Car;
 
 @FunctionalInterface
-public interface CarPredicate {
+public interface CarPredicate{
     boolean test(Car car);
 }
