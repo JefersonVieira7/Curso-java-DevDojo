bcdec3299a82a5baeff324cdd4ccf9ca41421f76
Jeferson Vieira <jeferson.vieira.dev@gmail.com>
2026-01-06 15:01:32 -0300

    Aula 208 - Streams pt 06 - Finding and Matching concluída

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstream/test/StreamTest06.java
@@ -0,0 +1,25 @@
+package academy.devdojo.maratonajava.javacore.ZZEstream.test;
+
+import academy.devdojo.maratonajava.javacore.ZZEstream.dominio.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamTest06 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 3.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no life", 2.99),
+            new LightNovel("fullmetal Alchemist", 5.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Monogarari", 4.00)
+    ));
+
+    public static void main(String[] args) {
+        System.out.println(lightNovels.stream().anyMatch(ln -> ln.getPrice() > 9));
+        System.out.println(lightNovels.stream().allMatch(ln -> ln.getPrice() > 0));
+        System.out.println(lightNovels.stream().noneMatch(ln -> ln.getPrice() > 0));
+    }
+}
